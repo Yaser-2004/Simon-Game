b8db34c14a6d7f2e520db903c2ff9d4c45ae7ad8
Yaser-2004 <133482087+Yaser-2004@users.noreply.github.com>
2024-07-06 12:29:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -109,3 +109,7 @@ window.addEventListener("keypress", () => {
     firstbuttonClick();
     console.log(gameColorSequence);
 })
+
+alert(
+    "INSTRUCTIONS FOR SIMON GAME:\n 1. START the game: Press any key on your keyboard to START the game. \n 2. Repeat the signal: Simon will give the first signal. Repeat the signal by pressing the same color button. \n 3. Add to the sequence: Simon will duplicate the first signal and add one. Repeat these two signals by pressing the same color buttons, in ORDER. \n 4. Continue playing: Continue playing as long as you can repeat each sequence of signals correctly. \n 5. GAME OVER: If you make an error, you'll hear the “RAZZ” sound. This means the game is over. \n WHEN YOU GET GAME OVER, RELOAD THE PAGE AGAIN."
+)
